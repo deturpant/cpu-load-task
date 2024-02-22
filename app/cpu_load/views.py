baf40ac4014eab1af83a716ce476cd14a489c15b
deturpant <vlad3032.kozlov@yandex.ru>
2024-02-22 16:32:27 +0300

    Added ListViewCPULoad class

---
+++ app/cpu_load/views.py
@@ -0,0 +1,16 @@
+from aiohttp import web
+
+from app.cpu_load.models import CPULoad
+
+
+class ListViewCPULoad(web.View):
+    async def get(self):
+        cpu_loads = await CPULoad.query.order_by(CPULoad.timestamp.desc()).gino.all()
+        data = []
+        for item in cpu_loads:
+            data.append({
+                "timestamp": item.timestamp,
+                "value": item.value,
+                "id": item.id
+            })
+        return web.json_response(data={"cpu_loads": data}, status=200)
