c819c78e4d9aed029b11e1404dd475b2490d3622
deturpant <vlad3032.kozlov@yandex.ru>
2024-02-22 15:51:31 +0300

    added additional imports to main, added setup accessors

--- app/store/database/models.py
+++ app/store/database/models.py
@@ -1,3 +1,7 @@
 from gino import Gino
 
+from app.store.database.accessor import PostgresAccessor
+
 db = Gino()
+
+database_accessor = PostgresAccessor()
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from aiohttp import web
 
 from app.config import config
+from app.store.database.models import database_accessor
 
 
-def setup_app(application):
-    setup_config(application)
+def setup_accessors(application):
+    database_accessor.setup(application)
 
 
 def setup_config(application):
     application['config'] = config
 
 
+def setup_app(application):
+    setup_config(application)
+    setup_accessors(application)
+
+
 app = web.Application()
 
 if __name__ == '__main__':
     setup_app(app)
-    web.run_app(app)
+    web.run_app(app, port=config['common']['port'])
